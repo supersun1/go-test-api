1704a33d3211f1f539d428fdb5503bb427637522
Dylan Sun <dylan_sun@intuit.com>
2019-02-22 14:55:23 -0800

    math funcs

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/supersun1/go-test-api
---
+++ myMath.go
@@ -0,0 +1,17 @@
+package main
+
+func NumSquared(num int) int {
+	tempNum := num
+	
+	tempNum = num * num
+
+	return tempNum
+}
+
+func NumCubed(num int) int {
+	tempNum := num
+
+	tempNum = num * num * num
+
+	return tempNum
+}	
